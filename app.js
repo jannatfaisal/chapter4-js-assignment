11ec6e99bdba217d19b684dcc0360d8173dff711
jannatfaisal <118313474+jannatfaisal@users.noreply.github.com>
2024-08-11 23:17:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+// chp 4
+
+// Q1
+
+// declare:
+// var var1,var2,var3;
+
+// Declare 5 legal & 5 illegal variable names
+
+
+// Legal Variable Names
+
+
+// var userName,varage_1, vartotalAmount, varisValid, varMAX_VALUE;
+
+
+// Illegal Variable Names
+// var 1stName ("cannot start with a number")
+// varuser name ("cannot contain spaces")
+// var (reserved keyword)
+// var@value ("cannot contain special characters except $ and _")
+// var if ("reserved keyword")
+
+
+
+    // Q3
+
+
+    // var heading=("Rules for naming JS variables:");
+
+    // document.write(heading +  "<br>" +  "<br>" +  "<br>"+  "<br>");
+
+   
+
+    // var b=(" 1)Variable names can only contain numbers, $,and  underscore For example $my_1stVariable");
+
+    // document.write(b  +  "<br>" +  "<br>" +  "<br>");
+
+    // var c=(" 2)Variables must begin with a letter,$ or For example $name, _name or name");
+
+    // document.write(c +  "<br>" +  "<br>" +  "<br>");
+
+    // var d=("3)Variable names are case sensitive");
+
+    // document.write(d +  "<br>" +  "<br>" +  "<br>");
+
+    // var e=("4)Variable names should not be JS keywords");
+    // document.write(e);
